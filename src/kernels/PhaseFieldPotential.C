24aeda98c694751c3e1f2d45d317578f04503803
micahjohnson150 <micah.johnson150@gmail.com>
2014-06-02 16:59:11 -0600

    Changed the sign of the equations from negative to positive.

--- src/kernels/PhaseFieldPotential.C
+++ src/kernels/PhaseFieldPotential.C
@@ -18,7 +18,7 @@ PhaseFieldPotential::computeDFDOP(PFFunctionType type)
   switch (type)
   {
   case Residual:
-    return _u[_qp]*_u[_qp]*_u[_qp] - _u[_qp] ;
+    return -_u[_qp]*_u[_qp]*_u[_qp] + _u[_qp] ;
 
   case Jacobian:
     return _phi[_j][_qp]*(3*_u[_qp]*_u[_qp] - 1. );
