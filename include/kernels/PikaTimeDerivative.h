ecd72ab78666244d6045649eeeaacd32e6850cf5
Andrew E Slaughter <andrew.slaughter@inl.gov>
2014-07-03 22:34:03 -0600

    Re-factored PikaTimeDerivative

--- include/kernels/PikaTimeDerivative.h
+++ include/kernels/PikaTimeDerivative.h
@@ -1,5 +1,5 @@
 #ifndef PIKATIMEDERIVATIVE_H
-#define PIKATIMEDERIVATIVE_H 
+#define PIKATIMEDERIVATIVE_H
 #include "TimeDerivative.h"
 #include "PropertyUserObjectInterface.h"
 #include "Material.h"
@@ -10,27 +10,65 @@ class PikaTimeDerivative;
 template<>
 InputParameters validParams<PikaTimeDerivative>();
 
+/**
+ * A coefficient time derivative Kernel
+ *
+ * This time Kernel allows to a coefficient to be applied to the time derivative, that
+ * coefficient may be either a scalar value or a scalar material property.
+ *
+ * This Kernel includes the ability to scale and offset the coefficient. The
+ * coefficient (material or scalar) is applied as:
+ *     (scale * coefficient + offset) * du/dt
+ *
+ * Also, include the ability to toggle the additional temporal scaling parameter (\xi)
+ * as defined by Kaempfer and Plapp (2009). This temporal scalling is applied in
+ * additions to the coefficient scaling:
+ *     xi * (scale * coefficient + offset) * du/dt
+ */
 class PikaTimeDerivative :
   public TimeDerivative,
   PropertyUserObjectInterface
 {
 public:
 
+  /**
+   * Class constructor.
+   */
   PikaTimeDerivative(const std::string & name, InputParameters parameters);
 
 protected:
+
+  /**
+   * Compute residual
+   * Utilizes TimeDerivative::computeQpResidual with applied coefficients and scaling
+   */
   virtual Real computeQpResidual();
 
+  /**
+   * Compute residual
+   * Utilizes TimeDerivative::computeQpJacobian with applied coefficients and scaling
+   */
   virtual Real computeQpJacobian();
 
+  /// Flag indicating to use material property rather than scalar coefficient
   const bool _has_material;
-  MaterialProperty<Real> * _material_coefficient;
-  const Real _offset;
-  const Real _scale;
-  Real _coefficient;
-  const bool & _has_time_scale;
-  Real _time_scale;
 
-private:
+  /// Pointer to material property to utilize as a coefficient
+  /* This must be a pointer because getMaterialProperty returns a const reference and
+   * this member is optional, so NULL is a valid value */
+  MaterialProperty<Real> * _material_coefficient;
+
+  /// Scalar coefficient
+  const Real _coefficient;
+
+  /// Coefficient offset
+  const Real _offset;
+
+  /// Coefficient offset
+  const Real _scale;
+
+  /// Time scaling factor (\xi)
+  const Real _time_scale;
 };
+
 #endif //PIKATIMEDERIVATIVE
--- src/kernels/PikaTimeDerivative.C
+++ src/kernels/PikaTimeDerivative.C
@@ -4,12 +4,11 @@ template<>
 InputParameters validParams<PikaTimeDerivative>()
 {
   InputParameters params = validParams<TimeDerivative>();
-  params.addParam<std::string>("property", "The name of the material property to be a coefficient on the time derivative. Cannot be specified simultaneaously as a coefficient");
-  params.addParam<Real>("offset", 0.0, "Offset added to the coefficient");
-  params.addParam<Real>("scale", 1.0, "Multiplier applied to the coefficient");
-  params.addParam<Real>("coefficient", "Constant scalar coefficient alternate to a material property coefficient. Cannot be specified simultaneously as property.");
-  params.addCoupledVar("differentiated_variable", "Variable actually being differentiated");
-  params.addParam<bool>("use_temporal_scaling",false, "Temporally scale this kernal with a value specified in PikaMaterials");
+  params.addParam<std::string>("property", "The name of the material property to be a coefficient on the time derivative. Cannot be specified simultaneously with a coefficient");
+  params.addParam<Real>("offset", 0.0, "Offset added to the coefficient (material and scalar");
+  params.addParam<Real>("scale", 1.0, "Multiplier applied to the coefficient (material and scalar");
+  params.addParam<Real>("coefficient", "Constant scalar coefficient alternate to a material property coefficient. Cannot be specified simultaneously with property.");
+  params.addParam<bool>("use_temporal_scaling", false, "Temporally scale this Kernel with a value specified in PikaMaterials");
   return params;
 }
 
@@ -17,51 +16,43 @@ PikaTimeDerivative::PikaTimeDerivative(const std::string & name, InputParameters
     TimeDerivative(name, parameters),
     PropertyUserObjectInterface(name,parameters),
     _has_material(isParamValid("property")),
-    _material_coefficient(NULL),
+    _material_coefficient(_has_material ? &getMaterialProperty<Real>(getParam<std::string>("property")) : NULL),
+    _coefficient(isParamValid("coefficient") ? getParam<Real>("coefficient") : 0.0),
     _offset(getParam<Real>("offset")),
     _scale(getParam<Real>("scale")),
-    _coefficient(0.0),
-    _has_time_scale(getParam<bool>("use_temporal_scaling")),
-    _time_scale(0.0)
-    
+    _time_scale(getParam<bool>("use_temporal_scaling") ? _property_uo.getParam<Real>("temporal_scaling") : 1.0)
+
 {
+  // Produce an error if both material and coefficient are defined
   if (_has_material && isParamValid("coefficient"))
     mooseError("A material property and a coefficient were specified in a PikaTimeDerivative kernel. Specify only one of them.");
 
+  // Produce an error if neither coefficient or material property are defined
   else if( !_has_material && !isParamValid("coefficient"))
     mooseError(" Neither a material property or a coefficient were specified in a PikaTimeDerivative kernel. Specify only one of them.");
 
-  else if( _has_material)
-    _material_coefficient = &getMaterialProperty<Real>(getParam<std::string>("property"));
-
-  else
-    _coefficient = getParam<Real>("coefficient");
-
-   if(_has_time_scale)
-      _time_scale = _property_uo.getParam<Real>("temporal_scaling");
-
-   else
-     _time_scale = 1.0;
-
 }
-
 
 Real
 PikaTimeDerivative::computeQpResidual()
 {
-    if (_has_material)
-      return _time_scale *(_scale * ((*_material_coefficient)[_qp]) + _offset) * (_test[_i][_qp]) * TimeDerivative::computeQpResidual(); 
+  // Material property
+  if (_has_material)
+    return _time_scale *(_scale * ((*_material_coefficient)[_qp]) + _offset) * (_test[_i][_qp]) * TimeDerivative::computeQpResidual();
 
-    else
-      return _time_scale *(_scale * _coefficient + _offset) * (_test[_i][_qp]) * TimeDerivative::computeQpResidual(); 
+  // Scalar coefficient
+  else
+    return _time_scale *(_scale * _coefficient + _offset) * (_test[_i][_qp]) * TimeDerivative::computeQpResidual();
 }
 
 Real
 PikaTimeDerivative::computeQpJacobian()
 {
-    if (_has_material)
-     return _time_scale * (_scale * ((*_material_coefficient)[_qp]) + _offset) *  TimeDerivative::computeQpJacobian(); 
+  // Material property coefficient
+  if (_has_material)
+    return _time_scale * (_scale * ((*_material_coefficient)[_qp]) + _offset) *  TimeDerivative::computeQpJacobian();
 
-    else
-     return _time_scale *(_scale * _coefficient + _offset) *  TimeDerivative::computeQpJacobian(); 
+  // Scalar coefficient
+  else
+    return _time_scale *(_scale * _coefficient + _offset) *  TimeDerivative::computeQpJacobian();
 }
