ef8e400ddb4ccc211624b6ab1175ee25d6126659
Micah Johnson <micah.johnson150@gmail.com>
2014-07-17 20:53:15 -0600

    Clean up from the messy spatial rescale

--- src/base/PikaApp.C
+++ src/base/PikaApp.C
@@ -51,10 +51,6 @@
 // Actions
 #include "PikaMaterialAction.h"
 
-//Outputs
-#include "PikaDebug.h"
-
-
 template<>
 InputParameters validParams<PikaApp>()
 {
@@ -122,8 +118,6 @@ PikaApp::registerObjects(Factory & factory)
   // AuxKernels
   registerAux(ErrorFunctionAux);
 
-  //outputs
-  registerOutput(PikaDebug);
 }
 
 void
