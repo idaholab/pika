0260c0e1eb8a370bcd775c3013ecd796343e0198
Andrew E Slaughter <andrew.slaughter@inl.gov>
2014-08-02 22:06:10 -0600

    Updated TensorMobility to take property names

--- src/kernels/TensorDiffusion.C
+++ src/kernels/TensorDiffusion.C
@@ -16,25 +16,15 @@ InputParameters validParams<TensorDiffusion>()
   InputParameters params = validParams<Diffusion>();
   params += validParams<PropertyUserObjectInterface>();
   params += validParams<CoefficientKernelInterface>();
-  params.addCoupledVar("phase", "Phase-field variable");
-
-  params.addParam<bool>("use_temporal_scaling", false, "Use the temporal scaling factor");
-
-  params.addRequiredParam<std::string>("M_1_property_name", "Name of material property for first mobility coefficient");
-  params.addRequiredParam<std::string>("M_2_property_name", "Name of material property for second econd mobility coefficient");
-
-
+  params.addParam<std::string>("mobility_tensor", "The tensor form of mobility (Nicoli, 2011)");
   return params;
 }
 
 TensorDiffusion::TensorDiffusion(const std::string & name, InputParameters parameters) :
     Diffusion(name, parameters),
     PropertyUserObjectInterface(name,parameters),
-    _phase(coupledValue("phase")),
-    _grad_phase(coupledGradient("phase")),
-    _M_1(getMaterialProperty<Real>(getParam<std::string>("M_1_property_name"))),
-    _M_2(getMaterialProperty<Real>(getParam<std::string>("M_2_property_name"))),
-    _xi(getParam<bool>("use_temporal_scaling") ? _property_uo.temporalScale() : 1.0)
+    CoefficientKernelInterface(name, parameters),
+    _coef(getMaterialProperty<RealTensorValue>(getParam<std::string>("mobility_tensor")))
 {
 }
 
@@ -46,39 +36,11 @@ TensorDiffusion::~TensorDiffusion()
 Real
 TensorDiffusion::computeQpResidual()
 {
-  RealTensorValue M = mobilityTensor();
-  return _grad_test[_i][_qp] * _xi * M * _grad_u[_qp];
+  return coefficient(_qp) * _coef[_qp] * _grad_test[_i][_qp] * _grad_u[_qp];
 }
 
 Real
 TensorDiffusion::computeQpJacobian()
 {
-  RealTensorValue M = mobilityTensor();
-  return _grad_test[_i][_qp] * _xi * M *_grad_phi[_j][_qp];
-}
-
-RealTensorValue
-TensorDiffusion::mobilityTensor()
-{
-    //Mobility for PHI = {-1,1}
-  _M_parallel[_qp] = _M_1[_qp] * (1.0 + _phase[_qp]) / 2.0 + _M_2[_qp[ * (1.0 - _phase[_qp])/2.0;
-  _M_perpendicular[_qp] = 1.0 / ((1.0/_M_1[_qp]) * (1 + _phase[_qp])/2.0 + (1.0/_M_2[_qp]) * ((1.0 - _phase[_qp])/2.0));
-
-  RealTensorValue nxn = normalOutputProduct();
-  return _M_perpendicular[_qp]*nxn + _M_parallel[_qp]*(_identity - nxn);
-
-                                                                       }
-RealTensorValue
-TensorDiffusion::normalOutputProduct()
-{
-
-  RealVectorValue n = _grad_phase[_qp] / _grad_phase[_qp].size();
-  RealTensorValue nxn;
-
-  for (unsigned int i = 0; i < LIBMESH_DIM; ++i)
-    for (unsigned int j = 0; j < LIBMESH_DIM; ++j)
-      nxn(i,j) = n(i)*n(j);
-
-  return nxn;
-
+  return coefficient(_qp) * _coef[_qp] * _grad_test[_i][_qp] * _grad_phi[_j][_qp];
 }
