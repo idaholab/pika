b32d6c1399addb6b1dca1edb80ed22a7518e3294
Andrew E Slaughter <andrew.slaughter@inl.gov>
2019-02-14 16:17:21 -0700

    Update deprecated functions

--- include/base/PikaApp.h
+++ include/base/PikaApp.h
@@ -28,8 +28,7 @@ public:
   PikaApp(InputParameters parameters);
   virtual ~PikaApp();
   static void registerApps();
-  static void registerObjects(Factory & factory);
-  static void associateSyntax(Syntax & syntax, ActionFactory & action_factory);
+  static void registerAll(Factory &, ActionFactory &, Syntax &);
 };
 
 #endif /* PIKAAPP_H */
